bbc4c6e55e19289f879a59abc2198ca5e3ee4707
Miguel Besas <rmgb222@yahoo.com.ph>
2018-06-02 09:10:01 +0800

    moved files to new folder

---
+++ UnityProject/Assets/imagineAR/Scripts/Editor/imagineARControllerEditor.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+[CustomEditor(typeof(imagineARController))]
+public class imagineARControllerEditor : Editor {
+
+	imagineARController _target;
+
+	public void OnEnable(){
+		_target = (imagineARController)target;
+	}
+
+	public override void OnInspectorGUI(){
+		DrawDefaultInspector ();
+
+		if (GUILayout.Button ("Build")) {
+			_target.Build ();
+		}
+		if (GUILayout.Button ("Initialize")) {
+			_target.Initialize ();
+		}
+		if (GUILayout.Button ("Train")) {
+			_target.TrainTracker ();
+		}
+	}
+}
